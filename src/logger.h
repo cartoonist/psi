2dacea9dffa849eaf988e1720b5a4590fb97bd78
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2017-09-26 13:30:56 +0200

    Add spdlog

--- src/logger.h
+++ src/logger.h
@@ -20,17 +20,70 @@
 #define LOGGER_H__
 
 #include "base.h"
+#include "options.h"
 
-// Setup Easylogging++ performance tracker to report in microseconds.
-#define ELPP_PERFORMANCE_MICROSECONDS
-// Prevent creation of default empty log file during pre-processing.
-#define ELPP_NO_DEFAULT_LOG_FILE
+#include "spdlog/spdlog.h"
 
-#ifdef NDEBUG
-// Disable Easylogging++ performance tracking logs.
-#define ELPP_DISABLE_PERFORMANCE_TRACKING
-#endif  // NDEBUG
 
-#include <easyloggingpp/src/easylogging++.h>
+namespace grem {
+  /**
+   *  @brief  Configure the main logger.
+   *
+   *  @param  options Program options.
+   *
+   *  It configures the "main" logger. The logger can be accessed by `get_logger`
+   *  method.
+   *
+   *  NOTE: The logger are multi-threaded.
+   */
+    inline void
+  config_logger ( const Options& options )
+  {
+    // Set to asynchronous mode.
+    spdlog::set_async_mode(8192);
+    std::vector< spdlog::sink_ptr > sinks;
 
+    if ( !options.nolog && !options.quiet ) {
+      if ( options.nocolor ) {
+        auto stdout_sink = spdlog::sinks::stdout_sink_mt::instance();
+        sinks.push_back( stdout_sink );
+      }
+      else {
+        auto color_sink = std::make_shared< spdlog::sinks::ansicolor_stdout_sink_mt >();
+        sinks.push_back( color_sink );
+      }
+    }
+
+    if ( !options.nolog && !options.nologfile ) {
+      auto simple
+        = std::make_shared< spdlog::sinks::simple_file_sink_mt >( options.log_path );
+      sinks.push_back( simple );
+    }
+
+    auto main_logger
+      = std::make_shared< spdlog::logger >( "main", begin(sinks), end(sinks) );
+    if ( options.verbose ) {
+      main_logger->set_level( spdlog::level::info );
+    }
+    else {
+      main_logger->set_level( spdlog::level::warn );
+    }
+
+    spdlog::register_logger( main_logger );
+  }
+
+  /**
+   *  @brief  Get the logger by name.
+   *
+   *  @param  name The name of the requested logger.
+   *  @return shared pointer to the logger.
+   *
+   *  It is a wrapper function for `spdlog::get` method.
+   */
+    inline std::shared_ptr<spdlog::logger>
+  get_logger( const std::string& name )
+  {
+    return spdlog::get( name );
+  }  /* -----  end of function get_logger  ----- */
+}  /* -----  end of namespace grem  ----- */
 #endif  // LOGGER_H__
