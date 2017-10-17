f5066771f8b6e700a6e73fcc115588bb166d0620
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2017-10-17 23:15:46 +0200

    [minor] points -> loci

--- src/grem.cc
+++ src/grem.cc
@@ -80,7 +80,7 @@ template< typename TMapper, typename TSet >
   report( TMapper& mapper, TSet& covered_reads, unsigned long long int found )
   {
     auto log = get_logger( "main" );
-    log->info( "Total number of starting points: {}", mapper.get_starting_loci().size() );
+    log->info( "Total number of starting loci: {}", mapper.get_starting_loci().size() );
     log->info( "Total number of seeds found: {}", found );
     log->info( "Total number of reads covered: {}", covered_reads.size() );
     log->info( "Total number of 'godown' operations: {}",
@@ -160,11 +160,11 @@ template< typename TIndexSpec  >
     prepare_paths_index( paths, mapper, paths_index_file, path_num );
 
     log->info( "Selecting starting loci..." );
-    /* Locate starting points. */
+    /* Locate starting loci. */
     mapper.add_all_loci( paths, seed_len, start_every );
     log->info( "Selected starting loci in {} us.",
         Timer::get_duration( "add-starts" ).count() );
-    log->info( "Number of starting points selected (in {} nodes): {}",
+    log->info( "Number of starting loci selected (in {} nodes): {}",
         mapper.get_vargraph()->node_count, mapper.get_starting_loci().size() );
 
     // :TODO:Tue Oct 17 22:37:\@cartoonist: save starting points as a part of paths index?
@@ -228,7 +228,7 @@ template< typename TIndexSpec  >
             Timer::get_duration( "paths-seed-find" ).count() );
         log->info( "Total number of seeds found on paths: {}", found - pre_found );
         log->info( "Traversing..." );
-        /* Find seeds on variation graph by traversing starting points. */
+        /* Find seeds on variation graph by traversing starting loci. */
         mapper.traverse ( write );
         log->info( "Traversed in {} us.", Timer::get_duration( "traverse" ).count() );
       }
@@ -248,7 +248,7 @@ startup( const Options & options )
   log->info( "- Paths index file: '{}'", options.paths_index_file );
   log->info( "- Reads chunk size: {}", options.chunk_size );
   log->info( "- Reads index type: {}", index_to_str(options.index) );
-  log->info( "- Starting points interval: {}", options.start_every );
+  log->info( "- Starting loci interval: {}", options.start_every );
 
   log->info( "Opening file '{}'...", options.fq_path );
   SeqFileIn reads_infile;
@@ -342,7 +342,7 @@ setup_argparser( seqan::ArgumentParser& parser )
                                           seqan::ArgParseArgument::INTEGER, "INT"));
   setRequired(parser, "c");
 
-  // starting points
+  // starting loci interval
   addOption(parser, seqan::ArgParseOption("e", "start-every", "Start from every given "
                                           "number of loci in all nodes. If it is set to"
                                           " 1, it means start from all positions in a "
