a5b0611d12732b2e118fe139163375086418092a
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2018-10-17 10:23:45 +0200

    Update plot scripts

--- script/plot_kmerquery.R
+++ script/plot_kmerquery.R
@@ -6,7 +6,7 @@ library('optparse')
 # Default values
 in_file <- 'benchmark.csv'
 p_title <- 'K-mer query time'
-p_subtitle <- 'for simulated dataset "%s" from N. Deltacephalinicoli genome ("snd")'
+p_subtitle <- 'k = %s; for simulated dataset "%s" from N. Deltacephalinicoli genome ("snd")'
 # Command-line options parser
 option_list <- list(make_option(c('-f', '--file'), type='character', default=in_file, help='Input CSV file\n\t\t[default=\'%default\']', metavar='character'),
                     make_option(c('-t', '--title'), type='character', default=p_title, help='Plot title\n\t\t[default=\'%default\']', metavar='character'),
@@ -31,29 +31,31 @@ if (is.null(opt$readslen)) {
 d <- read.table(opt$file, sep=',', header=TRUE)
 # Generating a plot for each dataset
 for (cdataset in levels(d$dataset)) {
-    # Data subsetting
-    ds <- d[d$dataset == cdataset, c('seedlen', 'pathno', 'chunksize', 'patched', 'stepsize', 'seedfindingtime')]
-    ds <- na.omit(ds)
-    # Removing duplicates
-    uds <- unique(ds[order(ds$chunksize),])
-    col_readslen <- rep(opt$readslen, dim(uds)[1])
-    col_nofreads <- rep(opt$nofreads, dim(uds)[1])
-    # Converting dataframe to tibble
-    #t <- as_data_frame(uds)
-    t <- tibble(chunksize=factor(uds$chunksize),
-                stepsize=uds$stepsize,
-                type=factor(ifelse(uds$patched=='yes', 'Patched', 'Full')),
-                npath=factor(uds$pathno),
-                kmerquery=(uds$seedfindingtime / ((col_readslen - uds$seedlen + 1) * col_nofreads)))
-    g <- ggplot(t, aes(x=chunksize, y=kmerquery, fill=npath)) +
-         geom_col(colour='#444444', position=position_dodge()) +
-         facet_grid(type ~ stepsize, labeller=label_both) +
-         labs(title=opt$title,
-              subtitle=sprintf(opt$subtitle, cdataset),
-              x='Chunk size',
-              y=expression('K-mer query ('*mu*'s)'),
-              fill='Paths') +
-         scale_y_continuous(limit=c(0, max(t$kmerquery)*1.1), expand=c(0, 0)) +
-         theme_bw()
-    ggsave(paste('kmerquery_', cdataset, '.pdf', sep=''))
+    for (cseedlen in unique(d$seedlen)) {
+        # Data subsetting
+        ds <- d[d$dataset == cdataset & d$seedlen == cseedlen, c('pathno', 'chunksize', 'patched', 'stepsize', 'seedfindingtime')]
+        ds <- na.omit(ds)
+        # Removing duplicates
+        uds <- unique(ds[order(ds$chunksize),])
+        col_readslen <- rep(opt$readslen, dim(uds)[1])
+        col_nofreads <- rep(opt$nofreads, dim(uds)[1])
+        # Converting dataframe to tibble
+        #t <- as_data_frame(uds)
+        t <- tibble(chunksize=factor(uds$chunksize),
+                    stepsize=uds$stepsize,
+                    type=factor(ifelse(uds$patched=='yes', 'Patched', 'Full')),
+                    npath=factor(uds$pathno),
+                    kmerquery=(uds$seedfindingtime / (as.integer(col_readslen / cseedlen) * col_nofreads)))
+        g <- ggplot(t, aes(x=chunksize, y=kmerquery, fill=npath)) +
+             geom_col(colour='#444444', position=position_dodge()) +
+             facet_grid(type ~ stepsize, labeller=label_both) +
+             labs(title=opt$title,
+                  subtitle=sprintf(opt$subtitle, cseedlen, cdataset),
+                  x='Chunk size',
+                  y=expression('K-mer query ('*mu*'s)'),
+                  fill='Paths') +
+             scale_y_continuous(limit=c(0, max(t$kmerquery)*1.1), expand=c(0, 0)) +
+             theme_bw()
+        ggsave(paste('kmerquery_', cdataset, '_', cseedlen, '.pdf', sep=''))
+    }
 }
--- script/plot_locino.R
+++ script/plot_locino.R
@@ -7,7 +7,7 @@ library('optparse')
 # Default values
 in_file <- 'benchmark.csv'
 p_title <- 'Starting loci'
-p_subtitle <- 'for simulated dataset "%s" from N. Deltacephalinicoli genome ("snd")'
+p_subtitle <- 'k = %s; for simulated dataset "%s" from N. Deltacephalinicoli genome ("snd")'
 # Command-line options parser
 option_list <- list(make_option(c('-f', '--file'), type='character', default=in_file, help='Input CSV file\n\t\t[default=\'%default\']', metavar='character'),
                     make_option(c('-t', '--title'), type='character', default=p_title, help='Plot title\n\t\t[default=\'%default\']', metavar='character'),
@@ -20,30 +20,32 @@ opt <- parse_args(opt_parser)
 d <- read.table(opt$file, sep=',', header=TRUE)
 # Generating a plot for each dataset
 for (cdataset in levels(d$dataset)) {
-    # Data subsetting
-    ds <- d[d$dataset == cdataset, c('pathno', 'stepsize', 'patched', 'locino', 'uniqnodes')]
-    # Removing duplicates
-    uds <- unique(ds[order(ds$pathno),])
-    # Melting
-    uds <- melt(uds, id.vars=c('pathno', 'stepsize', 'patched'))
-    # Converting dataframe to tibble
-    #t <- as_data_frame(uds)
-    t <- tibble(npath=uds$pathno,
-                stepsize=factor(uds$stepsize),
-                type=factor(ifelse(uds$patched == 'yes', 'Patched', 'Full')),
-                variable=factor(ifelse(uds$variable == 'locino', 'Loci', 'Nodes')),
-                loci=uds$value)
-    g <- ggplot(t, aes(x=npath, y=loci, colour=stepsize, linetype=variable, group=interaction(stepsize, variable))) +
-         geom_point(size=2, alpha=0.7) +
-         geom_line(alpha=0.7) +
-         facet_grid(. ~ type) +
-         labs(title=opt$title,
-              subtitle=sprintf(opt$subtitle, cdataset),
-              x='Paths',
-              y='Counts',
-              colour='Step size',
-              linetype='Starting') +
-         scale_x_continuous(breaks=unique(t$npath)) +
-         theme_bw()
-    ggsave(paste('locino_', cdataset, '.pdf', sep=''))
+    for (cseedlen in unique(d$seedlen)) {
+        # Data subsetting
+        ds <- d[d$dataset == cdataset & d$seedlen == cseedlen, c('pathno', 'stepsize', 'patched', 'locino', 'uniqnodes')]
+        # Removing duplicates
+        uds <- unique(ds[order(ds$pathno),])
+        # Melting
+        uds <- melt(uds, id.vars=c('pathno', 'stepsize', 'patched'))
+        # Converting dataframe to tibble
+        #t <- as_data_frame(uds)
+        t <- tibble(npath=uds$pathno,
+                    stepsize=factor(uds$stepsize),
+                    type=factor(ifelse(uds$patched == 'yes', 'Patched', 'Full')),
+                    variable=factor(ifelse(uds$variable == 'locino', 'Loci', 'Nodes')),
+                    loci=uds$value)
+        g <- ggplot(t, aes(x=npath, y=loci, colour=stepsize, linetype=variable, group=interaction(stepsize, variable))) +
+             geom_point(size=2, alpha=0.7) +
+             geom_line(alpha=0.7) +
+             facet_grid(. ~ type) +
+             labs(title=opt$title,
+                  subtitle=sprintf(opt$subtitle, cseedlen, cdataset),
+                  x='Paths',
+                  y='Counts',
+                  colour='Step size',
+                  linetype='Starting') +
+             scale_x_continuous(breaks=unique(t$npath)) +
+             theme_bw()
+        ggsave(paste('locino_', cdataset, '_', cseedlen, '.pdf', sep=''))
+    }
 }
