288b94002720219cef7b6221a0ca9c1cbe6a3e1b
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2020-05-27 18:26:36 +0200

    Fix dependency handling
    - Removed `sdsl` since it has been already handled by `gum`
    - Update `SeqAn` inclusion procedure based on a project by seqan team.
    - Handle `kseq++` when it is not installed on target system.
    - Add `Catch2` to target link libraries of test binary.

--- ext/external.cmake
+++ ext/external.cmake
@@ -6,17 +6,20 @@ if(NOT SeqAn_FOUND)
   execute_process(
     COMMAND git submodule update --init --recursive -- ${SEQAN_SOURCE_DIR}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-  add_subdirectory(${SEQAN_SOURCE_DIR})
+  set(CMAKE_INCLUDE_PATH ${SEQAN_SOURCE_DIR}/include ${CMAKE_INCLUDE_PATH})
+  set(CMAKE_PREFIX_PATH ${SEQAN_SOURCE_DIR}/util/cmake ${CMAKE_PREFIX_PATH})
+  set(CMAKE_MODULE_PATH ${SEQAN_SOURCE_DIR}/util/cmake ${CMAKE_MODULE_PATH})
+  find_package(SeqAn REQUIRED CONFIG)
 endif()
 
 # If found, but `SeqAn::SeqAn` target is not defined
 if(NOT TARGET SeqAn::SeqAn)
   add_library(SeqAn::SeqAn INTERFACE IMPORTED)
-  string(REGEX REPLACE "-D" "" SEQAN_DEFINITIONS_NOD "${SEQAN_DEFINITIONS}")
+  #string(REGEX REPLACE "-D" "" SEQAN_DEFINITIONS_NOD "${SEQAN_DEFINITIONS}")
   set_target_properties(SeqAn::SeqAn PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${SEQAN_INCLUDE_DIRS}"
-    INTERFACE_LINK_LIBRARIES "${SEQAN_LIBRARIES}"
-    INTERFACE_COMPILE_DEFINITIONS "${SEQAN_DEFINITIONS_NOD}")
+    INTERFACE_LINK_LIBRARIES "${SEQAN_LIBRARIES}")
+    #INTERFACE_COMPILE_DEFINITIONS "${SEQAN_DEFINITIONS_NOD}")
 endif()
 
 # When `gum` is not found
@@ -26,4 +29,13 @@ if(NOT TARGET gum::gum)
     COMMAND git submodule update --init --recursive -- ${GUM_SOURCE_DIR}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
   add_subdirectory(${GUM_SOURCE_DIR})
+endif()
+
+# When `kseq++` is not found
+if(NOT TARGET kseq++::kseq++)
+  set(KSEQPP_SOURCE_DIR ${PROJECT_SOURCE_DIR}/ext/kseq++)
+  execute_process(
+    COMMAND git submodule update --init --recursive -- ${KSEQPP_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  add_subdirectory(${KSEQPP_SOURCE_DIR})
 endif()
