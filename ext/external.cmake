e45f5e8472b871c961b86c70d383a22a49bb781e
Ali Ghaffaari <ali.ghaffaari@hhu.de>
2021-04-09 20:38:14 +0200

    Install the module defining SeqAn target

--- ext/external.cmake
+++ ext/external.cmake
@@ -18,15 +18,7 @@ if(NOT SeqAn_FOUND)
   find_package(SeqAn REQUIRED CONFIG)
 endif()
 
-# If found, but `SeqAn::SeqAn` target is not defined
-if(NOT TARGET SeqAn::SeqAn)
-  add_library(SeqAn::SeqAn INTERFACE IMPORTED)
-  #string(REGEX REPLACE "-D" "" SEQAN_DEFINITIONS_NOD "${SEQAN_DEFINITIONS}")
-  set_target_properties(SeqAn::SeqAn PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SEQAN_INCLUDE_DIRS}"
-    INTERFACE_LINK_LIBRARIES "${SEQAN_LIBRARIES}")
-    #INTERFACE_COMPILE_DEFINITIONS "${SEQAN_DEFINITIONS_NOD}")
-endif()
+include(SeqAnTarget)
 
 # When `gum` is not found
 if(NOT TARGET gum::gum)
