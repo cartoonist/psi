188f7c66a00e58f1472ece9b9c8a163a175a3901
Ali Ghaffaari <ali.ghaffaari@hhu.de>
2021-05-26 18:52:11 +0200

    Fix a minor issue in alice::dstats

--- tools/src/alice.cpp
+++ tools/src/alice.cpp
@@ -449,6 +449,7 @@ index_reference_paths( TPathSet& pathset, TGraph& graph )
   graph.for_each_path(
       [&nodes, &graph, &pathset]( auto rank, auto pid ) {
         typedef typename TPathSet::value_type path_type;
+        std::cerr << "! INFO Fetching reference path " << rank << "..." << std::endl;
         for ( auto n : graph.path( pid ) ) nodes.push_back( n );
         typename path_type::nodes_type cnodes( nodes );
         pathset.push_back( path_type( &graph, std::move( cnodes ) ) );
@@ -553,8 +554,10 @@ dstats( cxxopts::ParseResult& res )
   gaf::GAFRecord record1 = gaf::next( ifs );
   gaf::GAFRecord record2 = gaf::next( ifs );
   while ( record1 && record2 ) {
-    auto distance = distance_estimate( rpaths, graph, record1, record2 );
-    ost << distance << std::endl;
+    if ( record1.is_valid() && record2.is_valid() ) {
+      auto distance = distance_estimate( rpaths, graph, record1, record2 );
+      ost << distance << std::endl;
+    }
     record1 = gaf::next( ifs );
     record2 = gaf::next( ifs );
   }
@@ -739,7 +742,7 @@ main( int argc, char* argv[] )
     if ( command == "dstats" ) {
       dstats( res );
     }
-    if ( command == "analyse" ) {
+    else if ( command == "analyse" ) {
       analyse( res );
     }
     else {
