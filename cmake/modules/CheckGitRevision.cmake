5878cee4004167f2a806876b6a111b626558f634
Ali Ghaffaari <ali.ghaffaari@hhu.de>
2021-08-12 00:33:28 +0200

    Fix CMake CheckGitRevision module

--- cmake/modules/CheckGitRevision.cmake
+++ cmake/modules/CheckGitRevision.cmake
@@ -16,13 +16,17 @@ string(STRIP "${GIT_DESC}" GIT_DESC)
 
 # Check whether we got any revision
 if (NOT "${GIT_DESC}" STREQUAL "")
-  execute_process(COMMAND git rev-parse --abbrev-ref HEAD
+  execute_process(COMMAND git name-rev --name-only HEAD
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_BRANCH)
   execute_process(COMMAND git rev-parse --short HEAD
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_COMMIT_ID)
   execute_process(COMMAND git log -1 --format=%cd --date=format:%h\ %d\ %Y
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_COMMIT_DATE)
   string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+  string(REGEX MATCH "[^/]*$" GIT_BRANCH ${GIT_BRANCH})
   string(STRIP "${GIT_COMMIT_ID}" GIT_COMMIT_ID)
   string(STRIP "${GIT_COMMIT_DATE}" GIT_COMMIT_DATE)
   if ("${GIT_DESC}" STREQUAL "v${PROJECT_VERSION}"
